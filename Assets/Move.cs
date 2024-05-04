868e4244bf1894c308ea2bfaa2f9bfd788cd7dd9
colibri-the-bird <120297368+colibri-the-bird@users.noreply.github.com>
2024-05-04 15:22:07 +0500

    fwewff

---
+++ Assets/Move.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Rover;
+    public GameObject vila;
+
+    public GameObject lever1;
+    public GameObject lever2;
+
+    private Vector2 R1;
+    private float R2;
+    private float speed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        R1 = new Vector2(lever1.transform.localPosition.x, lever1.transform.localPosition.z);
+        R2 = lever2.transform.localPosition.x;
+
+        if (R1.x > 1) R1.x = 1;
+        if (R1.x < -1) R1.x = -1;
+        if (R1.y > 1) R1.y = 1;
+        if (R1.y < -1) R1.y = -1;
+        if (R2 > 1) R2 = 1;
+        if (R2 < -1) R2 = -1;
+        lever1.transform.localPosition = new Vector3(R1.x, 0, R1.y);
+        lever2.transform.localPosition = new Vector3(R2, 0, 0);
+        if (Mathf.Abs(R1.x) < 0.1) R1.x = 0;
+        if (Mathf.Abs(R1.y) < 0.1) R1.y = 0;
+        speed += R1.y * Time.deltaTime;
+        if (speed > 10) speed = 10;
+        if (speed < -10) speed = -10;
+        Rover.transform.position += Time.deltaTime * Rover.transform.forward * speed;
+        Rover.transform.rotation *= Quaternion.Euler(0, R1.x * speed * 15 * Time.deltaTime, 0);
+        vila.transform.localPosition = new Vector3(0, R2, 0);
+    }
+}
